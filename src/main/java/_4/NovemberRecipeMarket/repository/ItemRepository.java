807a6c5ad67237bc31e524e1606b91931db8ba3b
miji <mjkim2304@gmail.com>
2025-12-26 16:23:58 +0900

    fix: JPA Pessimistic Write lock을 사용한 동시성 문제 해결 (2024/11 수정)

--- src/main/java/_4/NovemberRecipeMarket/repository/ItemRepository.java
+++ src/main/java/_4/NovemberRecipeMarket/repository/ItemRepository.java
@@ -2,9 +2,13 @@ package _4.NovemberRecipeMarket.repository;
 
 import _4.NovemberRecipeMarket.domain.entity.Item;
 import _4.NovemberRecipeMarket.domain.entity.Seller;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,4 +17,11 @@ import java.util.Optional;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Optional<Item> findById(Long itemId);
     Page<Item> findAllBySeller(Seller seller, Pageable pageable);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT i FROM Item i WHERE i.id = :itemId")
+    Optional<Item> findByIdWithLock(@Param("itemId") Long itemId);
+
+    Page<Item> findByNameContaining(String keyword, Pageable pageable);
+
 }
