f6e6b70dffb72c970b1eaacaa981993ba47ec844
miji <mjkim2304@gmail.com>
2025-12-26 16:26:08 +0900

    chore

--- src/main/java/_4/NovemberRecipeMarket/domain/dto/recipe/RecipeRequest.java
+++ src/main/java/_4/NovemberRecipeMarket/domain/dto/recipe/RecipeRequest.java
@@ -3,14 +3,15 @@ package _4.NovemberRecipeMarket.domain.dto.recipe;
 import _4.NovemberRecipeMarket.domain.dto.recipeItem.RecipeItemRequest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class RecipeRequest {
     private String title;
     private String content;
     private List<RecipeItemRequest> ingredients;
-
 }
